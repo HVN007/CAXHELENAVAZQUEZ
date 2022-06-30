6b0977777f8b7f52103d4912f62c6d850e159dfe
HVN007 <106697946+HVN007@users.noreply.github.com>
2022-06-30 18:04:34 -0500

    Add files via upload

---
+++ Python/Tercer Trimestre/T3.py
@@ -0,0 +1,595 @@
+import random
+
+x = input("Cómo te llamas? ")
+print("")
+print("Bienvenid@ ", x)
+print("")
+input()
+print(
+    "El día de hoy es un día nublado y frío, pero estás feliz porque estás con tus amigxs."
+)
+input()
+print("")
+a = input("Cómo se llama tu amigx? ")
+b = input("Cómo se llama tu amigx? ")
+c = input("Cómo se llama tu amigx? ")
+print("")
+
+print(c, "- Oigan, ya me aburrí, no tienen ganas de ir a algún lado?")
+input()
+print(b, "- Tienes alguna idea?")
+input()
+print(c, "- Pues no sé, pregúntale a", x)
+input()
+print("")
+print("A dónde se te antoja ir?")
+print("a) Al parque")
+print("b) Al cine")
+print("c) A la plaza")
+input()
+print("")
+print(a, "- Bueno, vámonos caminando, al cabo está cerca")
+input()
+print("")
+print("Mientras se dirigen al lugar", a, "se da cuenta de algo...")
+input()
+print(a, "- Oigan soy yo, o ese carro rojo nos está siguiendo")
+input()
+print(b, "- Otra vez tú y tus alucinaciones.")
+input()
+print(a, "- No wey, sí nos están siguiendo...")
+input()
+print(a, "- Volteen disimuladamente")
+input()
+print("Elige: ")
+print("a) Voltear disimuladamente ")
+print("b) Voltear como exorcista")
+m = input("")
+
+if (m == "b"):
+    print("")
+    print(a, "- Wey, les dije disimuladamente")
+    input()
+    print(c, "- Upsi uwu")
+    input()
+    print(a, "- No manches, ya nos vieron, CORRELE GORDOO!!!!")
+    input()
+    print("Todos corren y se meten en el callejón obscuro")
+    input()
+    print("Pero ahí ya los estaban esperando...")
+    input()
+    input()
+    print("Con unas sospechosas armas…")
+    input()
+    print(x, "- *Diosito... eres tú?*")
+    input()
+    print("Todo se vuelve obscuro")
+
+else:
+    print("")
+    print(c, "- A no manches, sisierto")
+    input()
+    print(b, "- Corremos? O que nos cargue el payaso?")
+    input()
+
+    print("Elige:")
+    print("a) Correr")
+    print("b) Caminar")
+    h = input("")
+
+    if (h == "a"):
+        print("")
+        print("Todos corren y se meten en el callejón obscuro")
+        input()
+        print("Pero ahí ya los estaban esperando...")
+        input()
+        input()
+        print("Con unas sospechosas armas…")
+        input()
+        print(x, "- *Diosito... eres tú?*")
+        input()
+        print("Todo se vuelve obscuro")
+
+    else:
+        print("")
+        print(x, "- Metanse al Oxxo de allá")
+        input()
+        print(c, "- Vamos por Maruchan?")
+        input()
+        print(a, "- Sí wey, eres, o te haces?")
+        input()
+        print(b, "- No pelien")
+        input()
+        print("")
+        print("Se meten al Oxxo y esperan a que el coche pase")
+        input()
+        print(x, "- Ya pasó, vámonos")
+        input()
+        print(c, "- Y las maruchan?")
+        input()
+        print("*", a, "le da un sape a", c, "*")
+        input()
+        print("")
+        print("Todos salen de la tienda y siguen con su trayecto")
+        input()
+        print(
+            "Pero al pasar junto a un callejón, del otro lado de la calle, está el mismo coche rojo"
+        )
+        input()
+        print("")
+        print(b, "- Y ora?")
+        input()
+        print("")
+        print("Elige:")
+        print("a) Seguir caminando")
+        print("b) Meterse al callejón")
+        t = input("")
+
+        if (t == "b"):
+            print("")
+            print("Todos corren y se meten en el callejón obscuro")
+            input()
+            print("Pero ahí ya los estaban esperando...")
+            input()
+            input()
+            print("Con unas sospechosas armas…")
+            input()
+            print(x, "- *Diosito... eres tú?*")
+            input()
+            print("Todo se vuelve obscuro")
+
+        else:
+            print("")
+            print(x, "- Nomas caminen, y si nos matan, pues ya ni modo")
+            input()
+            print(
+                "Los del coche rojo los ven y bajan sus ventanillas, mostrando a 4 personas con la cara tapada, empuñando..."
+            )
+            input()
+            input()
+            print("...sartenes...")
+            input()
+            print(c, "- Me lleva la chin-")
+            input()
+            print(b, "- Esa boquita")
+            input()
+            print(a, "- Ya valió")
+            input()
+            print(x, "- En efecto, ya valimos")
+            input()
+            print(
+                "De un momento a otro entraron en pánico, obligándolos a correr..."
+            )
+            input()
+            input()
+            print("...pero ya era demasiado tarde...")
+            input()
+            print(x, "- *Solo fue un golpe*")
+            input()
+            print(x, "- *Diosito... eres tú?*")
+            input()
+            print("Todo se vuelve obscuro")
+input()
+input()
+input()
+print("")
+print("....")
+input()
+print("You Wake up")
+input()
+print(x, "- Where are we?")
+input()
+print(a, "- You brought us to Italy piece of @#£&*?")
+input()
+print(c, "- Where is the Eiffel Tower?")
+input()
+print(b, "- Stop it")
+input()
+print(x, "- Context please")
+input()
+print(b, "- I think we've been kidnapped")
+input()
+print(a, "- Oh really? If you don't tell me, I don't know")
+input()
+print(x, "- well, what's next?")
+input()
+print(c, "- Look for a way out")
+
+while True:
+    print("")
+    print("Where are you going to?")
+    print("a) To the bookshelf")
+    print("b) To the table")
+    print("c) To the closet(the one you haven't come out of)")
+    u = input("")
+
+    if (u == "a"):
+        print("")
+        print("You walk to the bookshelf, nothing anormal")
+        input()
+        print(
+            "You touch all the furniture just to check if there's a hidden door or something"
+        )
+        input()
+        print("Nothing")
+        input()
+        print("Just books, trophies, portraits and a chess game")
+        input()
+        print("")
+        continue
+
+    elif (u == "b"):
+        print("")
+        print(
+            "You walk towards the table, that it is on the center of the room, that has a carts game"
+        )
+        input()
+        print(
+            "You seek for something on the downside of the table, trying to find a way out"
+        )
+        input()
+        print("Nothing")
+        input()
+        print("Just wood and a dusty, old carpet")
+        input()
+        print(
+            "You walk backwards, but suddenly fell, the carpet moved a little, letting you see some sort of door."
+        )
+        input()
+        print(x, "- Uhmm guys, i found an exit, but we need a key")
+        input()
+        print(x, "- Try to find something ")
+        input()
+        break
+
+    else:
+        print("")
+        print(
+            "You run towards the closet, hoping to find the entry to Narnia inside it"
+        )
+        input()
+        print(
+            "You open the closet, and find coats of different colours hanging there, a pair of shoes and a box"
+        )
+        input()
+        print("But no exit")
+        input()
+        print("")
+        continue
+
+print(x, "- Did you guys found something?")
+input()
+print(b, "- Just a dirty chimney")
+input()
+print(c, "- Like you?")
+input()
+print(b, "- ªHey! Thats not true, i have showered today with Pantene")
+input()
+print(x, "- Stop it! Did you find something suspicious?")
+input()
+print(c, "- Mmmm just some random dissected animals in the wall")
+input()
+print(b, "- And?")
+input()
+print(c, "- Well they have weird names")
+input()
+print(x, "- Like what?")
+input()
+print(
+    "You all walk towards that wall, to observe the name tags underneath the animals"
+)
+input()
+print(
+    c,
+    "- Look! This one is called 15, i didnt know that Umbrella Acaemy had animals"
+)
+input()
+print(a, "*Frustrated scream uwu* Are you really like that?")
+input()
+print(a, "- Aren't you tired?")
+input()
+print(c, "- Eh?")
+input()
+print(b, "- Stop fighting, amur i pas chikos")
+input()
+
+while True:
+    print("")
+    input()
+    print(b, "- What does 15 mean?")
+    input()
+    print("a) The people they have cheated on you")
+    print("b) The safe box code")
+    print("c) The time")
+    l = input("")
+
+    if (l == "a"):
+        print("")
+        input()
+        print(a, "- Just get over it! It's been 84 years since it")
+        input()
+        print(
+            c,
+            "- I dont think it is that, no one cares about your personal life")
+        continue
+
+    if (l == "b"):
+        print(b, "- What safe box?")
+        input()
+        print(c, "- What could it have inside?")
+        input()
+        print(a, "- Perhaps a key")
+        input()
+        print(c, "- Maybe a chocolate?")
+        input()
+        print(x, "- Where is the safe box?")
+        input()
+        print("You split up to look for the safe box, but you dont find any")
+        input()
+        print(b, "-It isnt for that, innit?")
+        continue
+
+    if (l == "c"):
+        print(a, "-The hour?")
+        input()
+        print(x, "- It is the most logical thing")
+        input()
+        print(a, "- Look for a clock!")
+        input()
+        print(x, "- There is one in the wall!!")
+        input()
+        print(c, "- CUCU CUCU CUUUUUCUUUUUU")
+        input()
+        print(b, "- Now are you an actor?")
+        input()
+        print(a, "- Bruh, chill down you are not a freaking child")
+        input()
+        print(c, "- Ok cmon, put the hour in")
+        input()
+        break
+
+while True:
+    print("a)")
+    print(""" |
+ |___""")
+    print("b)")
+    print("""|  /
+| /
+|/""")
+    print("c)")
+    print("""||
+||
+||""")
+    r = input()
+
+    if (r == "a"):
+        break
+    if (r == "b"):
+        print(a, "- Didn't you learn the hours in elementary school?")
+        continue
+    if (r == "c"):
+        print(a, "- Where did you leave your brain cells?")
+        continue
+print("""_
+              ,/_\,
+           ,/_/   \_\,
+          /_/   ___   \_\
+
+       /_/     |(V)|     \_\
+   
+        |      .-.       |
+        |     / / \      |
+         |    \ \ /     |
+          |    '-'     |
+            '--,-,--'
+               | |
+               | |
+               | |
+               /\|
+               \/|
+               /\
+                \/""")
+print("CUCUCUCUCUCU!!!!")
+input()
+print(
+    "When the cucu bird comes out, you instantly see the key on the birds mouth."
+)
+input()
+print(x, "- A key!")
+input()
+print(a, "- For where?")
+input()
+print(c, "- Could it be for the exit?")
+input()
+print(x, "- Lets try")
+input()
+print("You move the carpet and you try to unlock the door with the key.")
+input()
+print("But it didnt work, the key didnt fit")
+input()
+print(b, "- Maybe it was to easy")
+input()
+print(a, "- Maybe in the closet?")
+input()
+print("You all walk towards the closet, and there are 6 cabinets")
+input()
+
+lista = [1, 2, 3, 4, 5, 6]
+eleccion = random.choice(lista)
+cajon = eleccion
+
+while True:
+    print("Which cabinet is the correct one?")
+    h = int(input("1,2,3,4,5 or 6?"))
+
+    if (h == cajon):
+        print("")
+        print(x, "- OMG It worked!")
+        break
+    else:
+        print("wrong")
+
+print("You open the cabinet and there is....")
+input()
+input()
+print("....nothing")
+input()
+print(a, "- What?!")
+input()
+print(b, "- Okay... there is nothing, let's go with the next one")
+input()
+input()
+print(x, "- No, wait")
+input()
+print(c, "-", x,
+      ", there is nothing, it is not in the cabinets, lets just move on")
+input()
+print(a, "- But look, the bottom of the cabinet has a diferent color")
+input()
+print(c, "- Ok that is true")
+input()
+print(x, "- But what does it mean?")
+input()
+print(b, "- Maybe we need to do something")
+input()
+print(a, "- Obviusly we need to do something there")
+input()
+print(c, "- What of we can open the bottom of the cabinet")
+input()
+print(a, "- Finally you say something rational and logical")
+input()
+print(b, "- That is quite impossible")
+input()
+print(x, "- Dont knock till' you try it")
+input()
+print(
+    "You try to open the bottom of the cabinet and you see a litle part in which  you can pull"
+)
+input()
+print(c, "- I told you there was something")
+input()
+print(x, "- Yeah, but what is that")
+input()
+print("You see a little paper that says " "Chicken Soup for the soul" "")
+input()
+print(c, "- Isn't it the name of a book?")
+input()
+print(x, "- It is, im actually reading it")
+input()
+print(a, "- I suppose that we need to find the book, right?")
+input()
+print(b, "- Yeah, that is the most logical thing")
+input()
+print("You go to the bookshelf trying to find the book")
+input()
+print(c, "- Where do we start?")
+input()
+print(x, "- You go here and", a, ", you go right there")
+input()
+print(a, "- Okay :)")
+input()
+print(c, "- Okay")
+input()
+print(b, "- And me?")
+input()
+print(x, "You try to look for the book on the top of the bookshelf")
+input()
+print(b, "- Okay")
+input()
+print("Everyone is looking for the godam book, but there were too many")
+input()
+print(c, "- We will never finish")
+input()
+print(x, "- What if we put all the books on the floor?")
+input()
+print(b, "- That could be easier")
+input()
+print(a, "- That is right, let's do it")
+input()
+print(x, "-", b, "and", a, "we are going to pass you the book, ok?")
+input()
+print(b, "- It sounds ok")
+input()
+print("While", x, "and", c, "are giving the books to the others,", b, "and", c,
+      "notice about something")
+input()
+print(b, "- I FOUND THE BOOK!!")
+input()
+print(c, "- I FOUND THE BOOK!!")
+input()
+print(x, "- Wait wot-")
+input()
+print(a, "- You two found the same book!?")
+input()
+print(b, "- I think we did.. isn't it obvious?")
+input()
+print(c, "- Yes we did...")
+input()
+print(x, "- So what's next")
+input()
+print(a, "- IDK")
+input()
+print(x, "-", a, "What is under your ankle?")
+input()
+print(a, "move the foot and there is another book with the same title ")
+input()
+print(a, "- ARE YOU SERIOUS?!!!?!?!?")
+input()
+print(x, "- Calm down, look, the books have different sizes")
+input()
+print(a, "- BUT THAT'S STUPID")
+input()
+print(b, "-", a, "please calm down ☮️")
+input()
+print(
+    c,
+    "- Yes, we need to find something, i'm gonna' look for something in the bookshelf"
+)
+input()
+print(
+    c,
+    "is looking in the bookshelf, when a book with the same title fall down")
+input()
+print(a, "- I don't want to play anymore!!")
+input()
+print(c, "- Hey hey, but look, there's a hole with the shape of a book")
+input()
+print(x, "- Maybe we need to enter the books there")
+input()
+
+lista2 = [1, 2, 3, 4]
+eleccion2 = random.choice(lista2)
+cajon2 = eleccion2
+while True:
+    u = int(input("Which book is the correct one? (1,2,3,4)"))
+    if (u == cajon2):
+        break
+    else:
+        print("It is bigger or smaller one")
+        continue
+print("Suddenly, something sounds, like a click")
+input()
+print(x, "- LMAO what was that?")
+input()
+print(a, "- LOOK, THE BOOKSHELF IS MOVINGG")
+input()
+print(a, "- WE ARE FREEEEEEE")
+input()
+print(
+    "When you think you all are free, you walk down the hallway just to see..."
+)
+input()
+print("... Another room")
+input()
+print("Another locked room")
+input()
+input()
+input()
+print("....")
+input()
+print("TO BE CONTUNUED...")
+input()
+print("RVA PT2 COMING SOON...")
+input()
+input()
+print("When mr. Roberto asks for it")
